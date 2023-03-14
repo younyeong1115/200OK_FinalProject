9c289fbdf0ff5e5252247e2acaccab569b5de75f
lee <lyy5223@naver.com>
2023-03-14 10:44:39 +0900

    first

---
+++ final_200OK/src/main/java/shop/myshop/Final200OkApplication.java
@@ -0,0 +1,13 @@
+package shop.myshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Final200OkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Final200OkApplication.class, args);
+	}
+
+}
