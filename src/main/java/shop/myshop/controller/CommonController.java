e63567513fbfa8780a5dff77489065713aebc262
lee <lyy5223@naver.com>
2023-03-27 10:11:27 +0900

    Add commonController / Update my-info.html,header.html

---
+++ src/main/java/shop/myshop/controller/CommonController.java
@@ -0,0 +1,23 @@
+package shop.myshop.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import shop.myshop.dto.UserDTO;
+
+@ControllerAdvice// 모든 컨트롤러에 적용
+public class CommonController {
+
+	@ModelAttribute//모든 컨트롤러에 추가
+	public void sharedData(Model model, HttpSession session) {
+	    UserDTO user = (UserDTO) session.getAttribute("user");
+	    if (user != null) {
+	        model.addAttribute("userName", user.getUserName());
+	    } else {
+	        model.addAttribute("userName", "");
+	    }
+	}
+}
