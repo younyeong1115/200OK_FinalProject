1406c2aa3caaedb6d1350e1541fe675a7c33fef5
kim <gksqlsk122@naver.com>
2023-04-03 09:40:00 +0900

    :hamster: Update Order

--- src/main/java/shop/myshop/controller/OrderController.java
+++ src/main/java/shop/myshop/controller/OrderController.java
@@ -27,9 +27,10 @@ import shop.myshop.service.ProductService;
 @RequestMapping("order")
 public class OrderController {
 
+
 	@Autowired
 	private OrdersService ordersService;
-	
+
 	@Autowired
 	private OrderItemService orderItemService;
 	
@@ -38,6 +39,8 @@ public class OrderController {
 	
 	@Autowired
 	private ProductService productService;
+
+
 
 
 	
@@ -96,5 +99,64 @@ public class OrderController {
 		return "product/success-payment";
 	}
 
+	
+	
+	
+	@GetMapping("paymentapi")
+	public String handlePaymentapiRequest(HttpSession session, @RequestParam("orderTotalprice") String orderTotalprice,
+			@RequestParam("deliveryName") String deliveryName, @RequestParam("deliveryMobile") String deliveryMobile,
+			@RequestParam("deliveryAdd1") String deliveryAdd1, @RequestParam("deliveryAdd2") String deliveryAdd2,
+			@RequestParam("deliveryAdd3") String deliveryAdd3, @RequestParam("deliveryMemo") String deliveryMemo) throws Exception {
+		
+		UserDTO userDTO = (UserDTO) session.getAttribute("user");
+		ModelMapper modelMapper = new ModelMapper();
+		User user = modelMapper.map(userDTO, User.class);
 
+		Orders order = new Orders();
+
+		order.setUserId(user);
+		Date regdate = new Date();
+		order.setOrderRegdate(regdate);
+		order.setOrderPayment("통합결제");
+		order.setOrderStatus("결제완료");
+		order.setOrderTotalprice(orderTotalprice);
+		order.setDeliveryName(deliveryName);
+		order.setDeliveryMobile(deliveryMobile);
+		order.setDeliveryAdd1(deliveryAdd1);
+		order.setDeliveryAdd2(deliveryAdd2);
+		order.setDeliveryAdd3(deliveryAdd3);
+		order.setDeliveryMemo(deliveryMemo);
+
+		ordersService.orderSave(order);
+		
+		//주문 품목 장바구니 삭제 ,오더 아이템 추가
+		List<Map<String, String>> selectedProducts = (List<Map<String, String>>) session.getAttribute("selectedProducts");
+		List<Integer> productCodeList = new ArrayList<>();
+		List<Integer> quantityList = new ArrayList<>();
+		for (Map<String, String> map : selectedProducts) {
+			productCodeList.add(Integer.parseInt(map.get("productCode")));
+			quantityList.add(Integer.parseInt(map.get("quantity")));
+		}
+		
+		for(int i =0; i<productCodeList.size();i++) {
+			cartService.deleteByUserIdAndProductCode(user.getUserId(), productCodeList.get(i));
+			OrderItem orderItem = new OrderItem();
+			orderItem.setOrderCode(order);
+			orderItem.setOrderitemQuantity(quantityList.get(i));
+			orderItem.setProductCode(productService.findByProductCode(productCodeList.get(i)));
+			orderItemService.orderItemrSave(orderItem);
+		}
+		
+		//selectedProducts 세션에서 삭제
+
+
+		return "product/success-paymentapi";
+	}
+
+	
+	
+	
+	
+	
+	
 }
