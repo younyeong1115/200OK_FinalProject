a8adf3813debd20539dff8a45b494efd3637f096
RimKim <122211031+RimKim@users.noreply.github.com>
2023-04-10 23:36:27 +0900

    Update MailConfig.java

--- src/main/java/shop/myshop/config/MailConfig.java
+++ src/main/java/shop/myshop/config/MailConfig.java
@@ -14,25 +14,25 @@ public class MailConfig {
     public JavaMailSender javaMailService() {
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
 
-        javaMailSender.setHost("smtp.naver.com"); // 메인 도메인 서버 주소 => 정확히는 smtp 서버 주소
+        javaMailSender.setHost("smtp.naver.com"); // smtp 서버 주소
         javaMailSender.setUsername(""); // 네이버 아이디
         javaMailSender.setPassword(""); // 네이버 비밀번호
 
-        javaMailSender.setPort(465); // 메일 인증서버 포트
+        javaMailSender.setPort(465); // 인증서버 포트
 
-        javaMailSender.setJavaMailProperties(getMailProperties()); // 메일 인증서버 정보 가져오기
+        javaMailSender.setJavaMailProperties(getMailProperties()); // 
 
         return javaMailSender;
     }
     
     private Properties getMailProperties() {
         Properties properties = new Properties();
-        properties.setProperty("mail.transport.protocol", "smtp"); // 프로토콜 설정
-        properties.setProperty("mail.smtp.auth", "true"); // smtp 인증
-        properties.setProperty("mail.smtp.starttls.enable", "true"); // smtp strattles 사용
-        properties.setProperty("mail.debug", "true"); // 디버그 사용
-        properties.setProperty("mail.smtp.ssl.trust","smtp.naver.com"); // ssl 인증 서버는 smtp.naver.com
-        properties.setProperty("mail.smtp.ssl.enable","true"); // ssl 사용
+        properties.setProperty("mail.transport.protocol", "smtp"); 
+        properties.setProperty("mail.smtp.auth", "true"); 
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.debug", "true"); 
+        properties.setProperty("mail.smtp.ssl.trust","smtp.naver.com"); 
+        properties.setProperty("mail.smtp.ssl.enable","true"); 
         return properties;
     }
 }
